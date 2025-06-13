3216afc680b0e88f66f1d82502473905c2ed808c
Silvia <silvia8gasparini@gmail.com>
2025-06-13 10:09:17 +0200

    create project

---
+++ src/test/java/it/epicode/U5W2D5/U5W2D5ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.U5W2D5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W2D5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
