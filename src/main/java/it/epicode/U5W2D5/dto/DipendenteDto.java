8d8ebf801d8713d57986bf9b84651f68819866ff
Silvia <silvia8gasparini@gmail.com>
2025-06-13 14:00:41 +0200

    project done

--- src/main/java/it/epicode/U5W2D5/dto/DipendenteDto.java
+++ src/main/java/it/epicode/U5W2D5/dto/DipendenteDto.java
@@ -1,12 +1,18 @@
 package it.epicode.U5W2D5.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class DipendenteDto {
+    @NotEmpty(message = "Username obbligatorio")
     private String username;
+    @NotEmpty(message = "Nome obbligatorio")
     private String nome;
+    @NotEmpty(message = "Cognome obbligatorio")
     private String cognome;
+    @NotEmpty(message = "Email obbligatoria")
     private String email;
     private String avatar;
 }
--- src/main/java/it/epicode/U5W2D5/dto/ViaggioDto.java
+++ src/main/java/it/epicode/U5W2D5/dto/ViaggioDto.java
@@ -1,13 +1,17 @@
 package it.epicode.U5W2D5.dto;
 
 import it.epicode.U5W2D5.enumeration.StatoViaggio;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
 public class ViaggioDto {
+    @NotEmpty(message = "Destinazione obbligatoria")
     private String destinazione;
+    @NotNull(message = "Data viaggio obbligatoria")
     private LocalDate dataViaggio;
     private StatoViaggio statoViaggio;
 }
